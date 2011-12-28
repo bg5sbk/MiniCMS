657e10854ce2ebd6ea4bf9cf820d2026d315b076
bg5sbk <bg5sbk@qq.com>
2011-12-28 20:52:57 +0800

    修改配置

--- mc-files/mc-conf.php
+++ mc-files/mc-conf.php
@@ -3,9 +3,9 @@ $mc_config = array (
   'site_link' => 'http://1234n.com/',
   'site_name' => '达达的主页',
   'site_desc' => '这是一个神奇的网站',
-  'user_name' => 'bg5sbk',
-  'user_pass' => 'njnjnj',
-  'user_nick' => '达达',
-  'disqus_shortname' => '1234n',
+  'user_name' => 'admin',
+  'user_pass' => 'admin',
+  'user_nick' => '某人',
+  'comment_code' => '',
 )
 ?>
