7d144b12c43273633c40e2579a4b0c07064cee94
bg5sbk <bg5sbk@qq.com>
2012-01-15 21:34:53 +0800

    添加RSS支持

---
+++ mc-files/mc-rss.php
@@ -0,0 +1,33 @@
+<?php header("Content-Type: text/xml"); ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; echo "\n"; ?>
+<rss version="2.0"
+  xmlns:content="http://purl.org/rss/1.0/modules/content/"
+  xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+  xmlns:dc="http://purl.org/dc/elements/1.1/"
+  xmlns:atom="http://www.w3.org/2005/Atom"
+  xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+  xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+>
+<channel>
+  <title><?php mc_site_name(); ?></title>
+  <link><?php mc_site_link(); ?></link>
+  <description><?php mc_site_desc(); ?></description>
+  <language>zh_CN</language>
+  <sy:updatePeriod>hourly</sy:updatePeriod>
+  <sy:updateFrequency>1</sy:updateFrequency>
+  <generator>http://1234n.com/?projects/minicms/</generator>
+<?php while (mc_next_post()) { ?>
+    <item>
+      <title><?php echo htmlspecialchars($mc_post['title']); ?></title>
+      <guid isPermaLink="false"><?php $mc_config['site_link'].'/?post/'.$mc_post_id; ?></guid>
+      <dc:creator><?php mc_nick_name(); ?></dc:creator>
+      <pubDate><?php mc_the_date(); ?> <?php mc_the_time(); ?></pubDate>
+<?php $tags = $mc_post['tags']; ?>
+<?php foreach ($tags as $tag) { ?>
+      <category><![CDATA[<?php echo htmlspecialchars($tag); ?>]]></category>
+<?php } ?>
+      <content:encoded><![CDATA[<?php mc_the_content();?>]]></content:encoded>
+    </item>
+<?php   } ?>
+</channel>
+</rss>
